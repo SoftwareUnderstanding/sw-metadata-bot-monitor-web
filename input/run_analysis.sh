1d6f77f73509c719f5522a74a43ef2bb7758ddca
Tom François <tom.francois@ntymail.com>
2026-04-09 18:10:12 +0200

    add some verbose in the script

--- input/run_analysis.sh
+++ input/run_analysis.sh
@@ -9,8 +9,15 @@ uv sync
 # run analysis using the bot's command line interface
 uv run sw-metadata-bot run-analysis --config-file $CONFIG_FILE
 
+echo "Analysis completed. Publishing results to respective repositories..."
+
 OUTPUT_DIR=$(uv run python -c "import read_config; config = read_config.read_config('$CONFIG_FILE'); print(read_config.get_output_folder(config))")
 # get latest subfolder created from config file
 LATEST_SUBFOLDER=$(ls -td $OUTPUT_DIR/*/ | head -1)
 
+# publish results to github pages
 uv run sw-metadata-bot publish --analysis-root $LATEST_SUBFOLDER/
+echo "Issues submitted to the repositories"
+
+# generate landing page
+uv run python generate_landing_page.py
